7b6978f006fbcd5ff5af798daefa56a510555160
Dimi <dimimytimain@gmail.com>
2026-02-05 13:55:31 +0200

    Updated Program.cs

--- AichmeeLab/Program.cs
+++ AichmeeLab/Program.cs
@@ -7,12 +7,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string baseAddr = builder.HostEnvironment.IsDevelopment()
+                  ? "http://localhost:7203/"//for testing
+                  : builder.HostEnvironment.BaseAddress;
 
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri("http://localhost:7203/")
-});
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddr) });
+
 
 builder.Services.AddScoped<ILibraryService, LibraryService>();
 
