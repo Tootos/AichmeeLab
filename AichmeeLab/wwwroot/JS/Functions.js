6b6b2636210ff665e89bd1d6320f4d5a19b8e1e9
Tootos <aichmeelab@gmail.com>
2026-05-25 14:14:42 +0300

    Adjustements + Bug fixes

--- AichmeeLab/wwwroot/JS/Functions.js
+++ AichmeeLab/wwwroot/JS/Functions.js
@@ -44,9 +44,9 @@ window.initializeSwipe = (dotNetHelper) => {
         const deltaX = touchEndX - touchStartX;
         const deltaY = Math.abs(touchEndY - touchStartY);
 
-        if (Math.abs(deltaX) > 70 && deltaY < 45) {
+        if (Math.abs(deltaX) > 70 && Math.abs(deltaY) < 45) {
             // Swipe Right from edge (Open)
-            if (deltaX > 0 && touchStartX < 220) {
+            if (deltaX > 0 && touchStartX < 320) {
                 dotNetHelper.invokeMethodAsync('SideBarToggle');
             } 
             // Swipe Left anywhere (Close)
